e36862a8eada0ca33c9c62bbc46934d0fac72713
Matteo Marco <matteomm00@gmail.com>
2021-08-21 14:49:08 +0200

    correggere funzione get su /utenti/name

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.get("/", (req, res) => {
   res.sendStatus(200);  //OK
 });
 
+//registra gli utenti
 app.post("/register/:name", (req, res) => {
   const n = req.params.name;
   
@@ -19,6 +20,49 @@ app.post("/register/:name", (req, res) => {
   console.log("User successfully registered: "+n);
 });
 
+//accedi all'endpoint degli utenti
+app.get("/utenti/:name", (req, res) =>{
+  const n = req.params.name;
+  let found = false;
+  
+  for(let i=0; i<utenti.length; i++){
+    if(utenti[i]==n){
+      found = true;
+    }
+  }
+  
+  if(found == false){
+    res.sendStatus(404); //not found
+    return;
+  }
+  
+  //found
+  const headerAccept = req.get('Accept');
+  console.log('Accept: ' + headerAccept);
+  
+  res.format({
+    'text/plain': () => {
+      res.type('text/plain').send("libro di "+n+" in db");
+    },
+    
+    'text/html': () => {
+      res.type('text/html').send("<html><body><h1> libro di "+n+"</h1></body></html>");
+    },
+    
+    'application/json': () => {
+      res.json({
+        name: n,
+        book: "titolo libro"
+      });
+    },
+    
+    default: () => {
+      res.sendStatus(406);
+    }
+  });
+});
+
+//app listen
 const listener = app.listen(process.env.PORT, () => {
   console.log("Listening on port" + listener.address().port);
 });
